741f5fd983751c8b56876cc2f8ef6dc521a370d0
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-12-26 20:51:25 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 
-ln -s "$SCRIPT_DIR/.zshrc" ~/.zshrc
-ln -s "$SCRIPT_DIR/nvim" ~/.config/nvim
-ln -s "$SCRIPT_DIR/tmux" ~/.config/tmux
+[[ -e "~/.zshrc" ]] || ln -s "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
+[[ -e "~/.config/nvim" ]] || ln -s "$SCRIPT_DIR/nvim" "$HOME/.config/nvim"
+[[ -e "~/.config/tmux" ]] || ln -s "$SCRIPT_DIR/tmux" "$HOME/.config/tmux"
 
-echo "Configs installed successfully" 
 exit 0
