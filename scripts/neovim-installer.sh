f469ebcc2680482bb3ac6bac659ff4de104e537a
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-02-08 00:49:14 +0200

    installing neovim

--- scripts/neovim-installer.sh
+++ scripts/neovim-installer.sh
@@ -8,10 +8,9 @@ sudo apt-get install ninja-build gettext cmake unzip curl
 # Install neovim
 git clone https://github.com/neovim/neovim "$HOME/neovim"
 cd "$HOME/neovim" || exit 1
-make CMAKE_BUILD_TYPE=RelWithDebInfo
 git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX="$HOME/neovim"
 rm -r build/  # clear the CMake cache
-make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
 make install
 export PATH="$HOME/neovim/bin:$PATH"
 
